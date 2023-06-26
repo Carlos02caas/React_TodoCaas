63e308de4eb11656c3aa7ac871f2d7c57592f70c
Carlos Andres Alavez <carlos_caas@hotmail.com>
2023-06-26 16:41:26 -0500

    Updates

---
+++ precache-manifest.424b1e692f745c7de7f12d016b627984.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "474862c02609bc04d1db",
+    "url": "/TodoCaas/static/css/main.9f20566b.chunk.css"
+  },
+  {
+    "revision": "474862c02609bc04d1db",
+    "url": "/TodoCaas/static/js/main.1de2879a.chunk.js"
+  },
+  {
+    "revision": "a302796c5a5d9907201a",
+    "url": "/TodoCaas/static/js/runtime~main.a0557bdc.js"
+  },
+  {
+    "revision": "bc3f0a0e24cb84ee1cdc",
+    "url": "/TodoCaas/static/js/2.3b7f2b86.chunk.js"
+  },
+  {
+    "revision": "c0ae17f020da0fc7eccc9824b7b83bf0",
+    "url": "/TodoCaas/static/media/AgregandoTodo.c0ae17f0.png"
+  },
+  {
+    "revision": "4dc171de8075bbe13085d98806b88e89",
+    "url": "/TodoCaas/index.html"
+  }
+];
